cc3f247334efc25edfaef39af5b49a096da1fe77
Wilson <Wilson@WILSON>
2021-03-17 18:11:13 -0300

    Delete seller

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -21,12 +21,24 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
 
         }
-
+            
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
+        }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
         }
     }
 }
+ 
